d84136721232df406d41a8b6cef8fab5ae8c4938
Shike-Cheng <69415470+Shike-Cheng@users.noreply.github.com>
2025-05-07 16:04:18 +0800

    Update utils.py

--- graph_repair/utils.py
+++ graph_repair/utils.py
@@ -185,20 +185,8 @@ def update_total_optim_dict(total_optim_dict, optim_dict):
             total_optim_dict[key][3][2] = optim_dict[key][3][2]
     return total_optim_dict
 
-# stage_event_flow = [[[['TP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']], [['MP', 'SO', 'ST'], ['MP', 'SO', 'RF'], ['MP', 'MF', 'WR']], [['MP', 'MF', 'EX'], ['MP', 'SO', 'ST'], ['TP', 'SO', 'RF']], [['MP', 'MF', 'EX'], ['MP', 'SO', 'ST'], ['MP', 'SO', 'RF']], [['MP', 'SO', 'ST'], ['MP', 'SO', 'RF'], ['MP', 'MF', 'EX']], [['TP', 'MF', 'RD']], [['TP', 'MF', 'RD'], ['TP', 'SO', 'ST'], ['TP', 'SO', 'RF']], [['MP', 'SF', 'RD'], ['MP', 'SO', 'ST'], ['MP', 'SO', 'RF']]], [[['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['TP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']], [['MP_1', 'MF', 'WR'], ['TP', 'MP_2', 'FR'], ['MP_2', 'MF', 'EX']], [['TP', 'MF', 'WR'], ['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['MP', 'MF', 'EX'], ['MP', 'TP', 'FR']], [['MP', 'TP', 'FR'], ['TP', 'MF', 'EX']], [['TP', 'MF', 'RD']], [['MP', 'MF', 'EX']], [['TP', 'MF', 'EX']], [['MP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']]], [[['MP', 'SF', 'RD'], ['MP', 'SF', 'WR']], [['MP', 'MF', 'EX'], ['MP', 'TP', 'IJ']], [['MP', 'SF', 'RD'], ['MP', 'SF', 'WR'], ['TP', 'SF', 'EX']], [['MP', 'MF', 'CD']], [['TP', 'MF', 'CD']], [['MP', 'SF', 'RD'], ['MP', 'TF', 'WR'], ['MP', 'TP', 'FR']], [['MP', 'SF', 'CD'], ['MP', 'TP', 'FR']], [['MP', 'SF', 'RD'], ['MP', 'MF', 'WR']], [['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['TP_1', 'TP_2', 'FR'], ['TP_2', 'MF', 'EX']], [['MP', 'MF', 'EX'], ['MP', 'SF', 'RD'], ['MP', 'SF', 'WR']], [['MP_1', 'SF', 'RD'], ['MP_1', 'SF', 'WR'], ['MP_1', 'MP_2', 'FR'], ['MP_2', 'MF', 'EX']], [['MP', 'MF', 'RD'], ['MP', 'TP', 'IJ']], [['MP_1', 'MP_2', 'FR'], ['MP_1', 'MF', 'RD'], ['MP_2', 'TP', 'IJ']]], [[['MP', 'TF', 'WR']], [['MP', 'SF', 'UK']], [['MP', 'SF', 'RD'], ['MP', 'SO', 'ST']], [['TP', 'SF', 'WR'], ['MP', 'SF', 'RD']], [['MP', 'TP', 'UK']], [['MP', 'MP', 'UK']], [['MP', 'MF', 'UK']]]]
 
-
-# 2 stage_event_flow = [[[['TP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']], [['MP', 'SO', 'ST'], ['MP', 'SO', 'RF'], ['MP', 'MF', 'WR']], [['MP', 'MF', 'EX'], ['MP', 'SO', 'ST'], ['TP', 'SO', 'RF']], [['MP', 'MF', 'EX'], ['MP', 'SO', 'ST'], ['MP', 'SO', 'RF']], [['MP', 'SO', 'ST'], ['MP', 'SO', 'RF'], ['MP', 'MF', 'EX']], [['TP', 'MF', 'RD']], [['TP', 'MF', 'RD'], ['TP', 'SO', 'ST'], ['TP', 'SO', 'RF']], [['MP', 'SF', 'RD'], ['MP', 'SO', 'ST'], ['MP', 'SO', 'RF']]], [[['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['TP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']], [['MP_1', 'MF', 'WR'], ['TP', 'MP_2', 'FR'], ['MP_2', 'MF', 'EX']], [['TP', 'MF', 'WR'], ['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['MP', 'MF', 'EX'], ['MP', 'TP', 'FR']], [['MP', 'TP', 'FR'], ['TP', 'MF', 'EX']], [['TP', 'MF', 'RD'], ['TP', 'MF', 'EX']], [['MP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']]], [[['MP', 'SF', 'RD'], ['MP', 'SF', 'WR']], [['MP', 'MF', 'EX'], ['MP', 'TP', 'IJ']], [['MP', 'SF', 'RD'], ['MP', 'SF', 'WR'], ['TP', 'SF', 'EX']], [['MP', 'MF', 'CD']], [['TP', 'MF', 'CD']], [['MP', 'SF', 'RD'], ['MP', 'TF', 'WR'], ['MP', 'TP', 'FR']], [['MP', 'SF', 'CD'], ['MP', 'TP', 'FR']], [['MP', 'SF', 'RD'], ['MP', 'MF', 'WR']], [['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['TP_1', 'TP_2', 'FR'], ['TP_2', 'MF', 'EX']], [['MP', 'MF', 'EX'], ['MP', 'SF', 'RD'], ['MP', 'SF', 'WR']], [['MP_1', 'SF', 'RD'], ['MP_1', 'SF', 'WR'], ['MP_1', 'MP_2', 'FR'], ['MP_2', 'MF', 'EX']], [['MP', 'MF', 'RD'], ['MP', 'TP', 'IJ']], [['MP_1', 'MP_2', 'FR'], ['MP_1', 'MF', 'RD'], ['MP_2', 'TP', 'IJ']]], [[['MP', 'TF', 'WR']], [['MP', 'SF', 'UK']], [['MP', 'SF', 'RD'], ['MP', 'SO', 'ST']], [['TP', 'SF', 'WR'], ['MP', 'SF', 'RD']], [['MP', 'TP', 'UK']], [['MP', 'MP', 'UK']], [['MP', 'TP', 'IJ']], [['MP', 'MF', 'UK']]]]
-
-
-# stage_event_flow = [[[['TP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']], [['MP', 'SO', 'ST'], ['MP', 'SO', 'RF'], ['MP', 'MF', 'WR']], [['MP', 'MF', 'EX'], ['MP', 'SO', 'ST'], ['TP', 'SO', 'RF']], [['MP', 'MF', 'EX'], ['MP', 'SO', 'ST'], ['MP', 'SO', 'RF']], [['MP', 'SO', 'ST'], ['MP', 'SO', 'RF'], ['MP', 'MF', 'EX']], [['TP', 'MF', 'RD']], [['TP', 'MF', 'RD'], ['TP', 'SO', 'ST'], ['TP', 'SO', 'RF']], [['MP', 'SF', 'RD'], ['MP', 'SO', 'ST'], ['MP', 'SO', 'RF']]], [[['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['TP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']], [['MP_1', 'MF', 'WR'], ['TP', 'MP_2', 'FR'], ['MP_2', 'MF', 'EX']], [['TP', 'MF', 'WR'], ['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['MP', 'MF', 'EX'], ['MP', 'TP', 'FR']], [['MP', 'TP', 'FR'], ['TP', 'MF', 'EX']], [['TP', 'MF', 'RD'], ['TP', 'MF', 'EX']], [['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']]], [[['MP', 'SF', 'RD'], ['MP', 'SF', 'WR']], [['MP', 'MF', 'EX'], ['MP', 'TP', 'IJ']], [['MP', 'SF', 'RD'], ['MP', 'SF', 'WR'], ['TP', 'SF', 'EX']], [['MP', 'MF', 'CD']], [['TP', 'MF', 'CD']], [['MP', 'SF', 'RD'], ['MP', 'TF', 'WR'], ['MP', 'TP', 'FR']], [['MP', 'SF', 'CD'], ['MP', 'TP', 'FR']], [['MP', 'SF', 'RD'], ['MP', 'MF', 'WR']], [['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['TP_1', 'TP_2', 'FR'], ['TP_2', 'MF', 'EX']], [['MP', 'MF', 'EX'], ['MP', 'SF', 'RD'], ['MP', 'SF', 'WR']], [['MP_1', 'SF', 'RD'], ['MP_1', 'SF', 'WR'], ['MP_1', 'MP_2', 'FR'], ['MP_2', 'MF', 'EX']], [['MP', 'MF', 'RD'], ['MP', 'TP', 'IJ']], [['MP_1', 'MP_2', 'FR'], ['MP_1', 'MF', 'RD'], ['MP_2', 'TP', 'IJ']]], [[['MP', 'TF', 'WR']], [['MP', 'SF', 'UK']], [['MP', 'SF', 'RD'], ['MP', 'SO', 'ST']], [['TP', 'SF', 'WR'], ['MP', 'SF', 'RD']], [['MP', 'TP', 'UK']], [['MP', 'MP', 'UK']], [['MP', 'TP', 'IJ']], [['MP', 'MF', 'UK']]]]
-
-# stage_event_flow = [[[['TP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']], [['MP', 'SO', 'ST'], ['MP', 'SO', 'RF'], ['MP', 'MF', 'WR']], [['MP', 'MF', 'EX'], ['MP', 'SO', 'ST'], ['TP', 'SO', 'RF']], [['MP', 'MF', 'EX'], ['MP', 'SO', 'ST'], ['MP', 'SO', 'RF']], [['MP', 'SO', 'ST'], ['MP', 'SO', 'RF'], ['MP', 'MF', 'EX']], [['TP', 'MF', 'RD'], ['TP', 'SO', 'ST'], ['TP', 'SO', 'RF']], [['MP', 'SF', 'RD'], ['MP', 'SO', 'ST'], ['MP', 'SO', 'RF']]], [[['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['TP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']], [['MP_1', 'MF', 'WR'], ['TP', 'MP_2', 'FR'], ['MP_2', 'MF', 'EX']], [['TP', 'MF', 'WR'], ['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['MP', 'MF', 'EX'], ['MP', 'TP', 'FR']], [['MP', 'TP', 'FR'], ['TP', 'MF', 'EX']], [['MP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']]], [[['MP', 'SF', 'RD'], ['MP', 'SF', 'WR']], [['MP', 'MF', 'EX'], ['MP', 'TP', 'IJ']], [['MP', 'SF', 'RD'], ['MP', 'SF', 'WR'], ['TP', 'SF', 'EX']], [['MP', 'MF', 'CD']], [['TP', 'MF', 'CD']], [['MP', 'SF', 'RD'], ['MP', 'TF', 'WR'], ['MP', 'TP', 'FR']], [['MP', 'SF', 'CD'], ['MP', 'TP', 'FR']], [['MP', 'SF', 'RD'], ['MP', 'MF', 'WR']], [['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['TP_1', 'TP_2', 'FR'], ['TP_2', 'MF', 'EX']], [['MP', 'MF', 'EX'], ['MP', 'SF', 'RD'], ['MP', 'SF', 'WR']], [['MP_1', 'SF', 'RD'], ['MP_1', 'SF', 'WR'], ['MP_1', 'MP_2', 'FR'], ['MP_2', 'MF', 'EX']], [['MP', 'MF', 'RD'], ['MP', 'TP', 'IJ']], [['MP_1', 'MP_2', 'FR'], ['MP_1', 'MF', 'RD'], ['MP_2', 'TP', 'IJ']]], [[['MP', 'TF', 'WR']], [['MP', 'SF', 'UK']], [['MP', 'SF', 'RD'], ['MP', 'SO', 'ST']], [['TP', 'SF', 'WR'], ['MP', 'SF', 'RD']], [['MP', 'TP', 'UK']], [['MP', 'MP', 'UK']], [['MP', 'MF', 'UK']]]]
-
-# stage_event_flow = [[[['TP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']], [['MP', 'SO', 'ST'], ['MP', 'SO', 'RF'], ['MP', 'MF', 'WR']], [['MP', 'MF', 'EX'], ['MP', 'SO', 'ST'], ['TP', 'SO', 'RF']], [['MP', 'MF', 'EX'], ['MP', 'SO', 'ST'], ['MP', 'SO', 'RF']], [['MP', 'SO', 'ST'], ['MP', 'SO', 'RF'], ['MP', 'MF', 'EX']], [['TP', 'MF', 'RD']], [['TP', 'MF', 'RD'], ['TP', 'SO', 'ST'], ['TP', 'SO', 'RF']], [['MP', 'SF', 'RD'], ['MP', 'SO', 'ST'], ['MP', 'SO', 'RF']]], [[['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['TP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']], [['MP_1', 'MF', 'WR'], ['TP', 'MP_2', 'FR'], ['MP_2', 'MF', 'EX']], [['TP', 'MF', 'WR'], ['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['MP', 'MF', 'EX'], ['MP', 'TP', 'FR']], [['MP', 'TP', 'FR'], ['TP', 'MF', 'EX']], [['TP', 'MF', 'RD']], [['MP', 'MF', 'EX']], [['TP', 'MF', 'EX']], [['MP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']]], [[['MP', 'SF', 'RD'], ['MP', 'SF', 'WR']], [['MP', 'MF', 'EX'], ['MP', 'TP', 'IJ']], [['MP', 'SF', 'RD'], ['MP', 'SF', 'WR'], ['TP', 'SF', 'EX']], [['MP', 'MF', 'CD']], [['TP', 'MF', 'CD']], [['MP', 'SF', 'RD'], ['MP', 'TF', 'WR'], ['MP', 'TP', 'FR']], [['MP', 'SF', 'CD'], ['MP', 'TP', 'FR']], [['MP', 'SF', 'RD'], ['MP', 'MF', 'WR']], [['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['TP_1', 'TP_2', 'FR'], ['TP_2', 'MF', 'EX']], [['MP', 'MF', 'EX'], ['MP', 'SF', 'RD'], ['MP', 'SF', 'WR']], [['MP_1', 'SF', 'RD'], ['MP_1', 'SF', 'WR'], ['MP_1', 'MP_2', 'FR'], ['MP_2', 'MF', 'EX']], [['MP', 'MF', 'RD'], ['MP', 'TP', 'IJ']], [['MP_1', 'MP_2', 'FR'], ['MP_1', 'MF', 'RD'], ['MP_2', 'TP', 'IJ']]], [[['MP', 'TF', 'WR']], [['MP', 'SF', 'UK']], [['MP', 'SF', 'RD'], ['MP', 'SO', 'ST']], [['TP', 'SF', 'WR'], ['MP', 'SF', 'RD']], [['MP', 'TP', 'UK']], [['MP', 'TP', 'IJ']], [['MP', 'MP', 'UK']], [['MP', 'MF', 'UK']]]]
-
-stage_event_flow = [[[['TP', 'SO', 'ST'], ['TP', 'SO', 'RF']], [['MP', 'SO', 'ST'], ['MP', 'SO', 'RF'], ['MP', 'MF', 'WR']], [['MP', 'MF', 'EX'], ['MP', 'SO', 'ST'], ['TP', 'SO', 'RF']], [['MP', 'MF', 'EX'], ['MP', 'SO', 'ST'], ['MP', 'SO', 'RF']], [['MP', 'SO', 'ST'], ['MP', 'SO', 'RF'], ['MP', 'MF', 'EX']], [['TP', 'MF', 'RD']], [['TP', 'MF', 'RD'], ['TP', 'SO', 'ST'], ['TP', 'SO', 'RF']], [['MP', 'SF', 'RD'], ['MP', 'SO', 'ST'], ['MP', 'SO', 'RF']]], [[['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['TP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']], [['MP_1', 'MF', 'WR'], ['TP', 'MP_2', 'FR'], ['MP_2', 'MF', 'EX']], [['TP', 'MF', 'WR'], ['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['MP', 'MF', 'EX'], ['MP', 'TP', 'FR']], [['MP', 'TP', 'FR'], ['TP', 'MF', 'EX']], [['TP', 'MF', 'RD']], [['MP', 'MF', 'EX']], [['TP', 'MF', 'EX']], [['MP', 'SO', 'ST'], ['TP', 'SO', 'RF'], ['TP', 'MF', 'WR']]], [[['MP', 'SF', 'RD'], ['MP', 'SF', 'WR']], [['MP', 'MF', 'EX'], ['MP', 'TP', 'IJ']], [['MP', 'SF', 'RD'], ['MP', 'SF', 'WR'], ['TP', 'SF', 'EX']], [['MP', 'MF', 'CD']], [['TP', 'MF', 'CD']], [['MP', 'SF', 'RD'], ['MP', 'TF', 'WR'], ['MP', 'TP', 'FR']], [['MP', 'SF', 'CD'], ['MP', 'TP', 'FR']], [['MP', 'SF', 'RD'], ['MP', 'MF', 'WR']], [['TP', 'MP', 'FR'], ['MP', 'MF', 'EX']], [['TP_1', 'TP_2', 'FR'], ['TP_2', 'MF', 'EX']], [['MP', 'MF', 'EX'], ['MP', 'SF', 'RD'], ['MP', 'SF', 'WR']], [['MP_1', 'SF', 'RD'], ['MP_1', 'SF', 'WR'], ['MP_1', 'MP_2', 'FR'], ['MP_2', 'MF', 'EX']], [['MP', 'MF', 'RD'], ['MP', 'TP', 'IJ']], [['MP_1', 'MP_2', 'FR'], ['MP_1', 'MF', 'RD'], ['MP_2', 'TP', 'IJ']]], [[['MP', 'TF', 'WR']], [['MP', 'SF', 'UK']], [['MP', 'SF', 'RD'], ['MP', 'SO', 'ST']], [['TP', 'SF', 'WR'], ['MP', 'SF', 'RD']], [['MP', 'TP', 'UK']], [['MP', 'TP', 'IJ']], [['MP', 'MP', 'UK']], [['MP', 'MF', 'UK']]]]
-
+# stage_event_flow = [[x,x,x], []] # flag event definition
 
 class Reasonable_judgment(object):
     def __init__(self, nodes, edges, no_re_map):
@@ -209,7 +197,7 @@ class Reasonable_judgment(object):
 
     def find_object(self, behavior, start):
         for i in range(start, len(self.edges)):
-            if self.edges[i][0] == self.edges[start][0]:              # 以当前匹配到的sub
+            if self.edges[i][0] == self.edges[start][0]:
                 if self.node_type[self.edges[i][1]] == behavior[1][0:2] and self.no_re_map[self.edges[i][2]] == behavior[2]:
                     return True, i
         return False, None
@@ -217,7 +205,7 @@ class Reasonable_judgment(object):
     def find_subject(self, behavior, start):
         for i in range(start, len(self.edges)):
             # print(self.node_type[self.edges[i][0]])
-            if self.node_type[self.edges[i][0]] == behavior[0][0:2]:              # 以当前匹配到的sub
+            if self.node_type[self.edges[i][0]] == behavior[0][0:2]:
                 return True, i
         return False, None
 
@@ -227,34 +215,34 @@ class Reasonable_judgment(object):
         elif start == len(self.edges):
             return False, start
         else:
-            flag1, start1 = self.find_subject(behavior, start)                      # start1是指当前找到的主体的time
+            flag1, start1 = self.find_subject(behavior, start)
             if flag1:
-                flag2, start2 = self.find_object(behavior, start1)                  # 从start1开始找该主体对应动作的客体，找到了则当前对应start2
+                flag2, start2 = self.find_object(behavior, start1)
                 if flag2:
                     return self.factorial(behavior, start2, 1)
                 else:
-                    return self.factorial(behavior, start1 + 1, 0)                  # 客体没有找到就继续顺着当前主体找到的start1 + 1，继续找合适的主体和客体
+                    return self.factorial(behavior, start1 + 1, 0)
             else:
                 return False, start
 
     def knowledge_judge(self):
-        sum_flag = 0     # 四个阶段的夺旗标志
+        sum_flag = 0   
         stage_count = 0
-        ti_me = 0        # 大时间,阶段推进时间
+        ti_me = 0 
         sum_flow_index = []
         sum_edge_time_list = [[] for i in range(len(stage_event_flow))]
         for stage in stage_event_flow:
             flow_count = 0
-            flow_start_time = ti_me                    # 事件流时间
+            flow_start_time = ti_me               
             flow_flag_time = []
             flow_index_dict = {}
             edge_time_list = [[] for i in range(len(stage))]
-            for flow in stage:                         # 阶段里面的流
+            for flow in stage:                         
                 # current_node = self.edges[ti_me][0]
                 behavior_count = 0
-                # flow_start_time = ti_me                            # 时间戳
-                for behavior in flow:                                # 一种类型的流里面的动作判断
-                    be_start_time = flow_start_time                  # 动作推进时间
+                # flow_start_time = ti_me                            
+                for behavior in flow:                          
+                    be_start_time = flow_start_time
                     if be_start_time == len(self.edges) and behavior_count != len(flow):
                         break
                     else:
@@ -268,19 +256,13 @@ class Reasonable_judgment(object):
                         # print(be_start_time)
                         edge_time_list[flow_count].append(be_start_time)
                         behavior_count += 1
-                        flow_start_time = be_start_time + 1       # 满足某个阶段里面某个事件流的一个动作,时序向前推进
+                        flow_start_time = be_start_time + 1
                         continue
 
                 if behavior_count == len(flow):
                     flow_flag_time.append(flow_start_time)
                     flow_index_dict[flow_start_time] = flow_count
-                    # sum_flag += 1
-                    # ti_me = flow_start_time                       # 当全部满足某个阶段里面某个事件流的完整动作时,则从当前满足的事件流的时序开始,推进大时间,进行下一阶段的事件流匹配
-                    # print("该图在第{}阶段，第{}个事件流完全通过，通过了{}个动作".format(stage_count, flow_count, behavior_count))
-                    # break
-                # else:
-                    # flow_start_time = ti_me                       # 没有完全满足某个阶段里面某个事件流的全部动作,则时序重新回到大时间,重新匹配当前阶段其他类型的事件流
-                    # print("该图在第{}阶段，第{}个事件流没有完全通过，只通过了{}个动作".format(stage_count, flow_count, behavior_count))
+
                 flow_start_time = ti_me
                 flow_count += 1
 
